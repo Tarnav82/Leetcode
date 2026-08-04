a6ac9253491911079c5097df061b59972e48c49a
Trespasser <arnavtiwari3513@gmail.com>
2026-08-04 21:47:45 +0530

    Time: 6 ms (39.24%) | Memory: 47.2 MB (18.67%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.java
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -5,7 +5,7 @@ class Solution {
 
         for (int i = 0; i < nums.length - 1; i++) {
             int current = nums[i];
-            int next = nums[i + 1];
+       int next = nums[i + 1];
 
            
             int val = current + 1;
