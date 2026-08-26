c7197a6192dc7cd67adfcfdcf2f2164048d9c1c6
Trespasser <arnavtiwari3513@gmail.com>
2026-08-26 23:26:25 +0530

    Time: 3 ms (77.28%) | Memory: 46.6 MB (48.96%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,25 +1,44 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> roman = new HashMap<>();
-        roman.put('I', 1);
-        roman.put('V', 5);
-        roman.put('X', 10);
-        roman.put('L', 50);
-        roman.put('C', 100);
-        roman.put('D', 500);
-        roman.put('M', 1000);
         
-        int total = 0;
-        int n = s.length();
-        
-        for (int i = 0; i < n; i++) {
-            if (i + 1 < n && roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))) {
-                total -= roman.get(s.charAt(i));
-            } else {
-                total += roman.get(s.charAt(i));
+        int sum = 0;
+        int[] nums = new int[s.length()];
+
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            switch(c) {
+                case 'M':
+                    nums[i] = 1000;
+                    break;
+                case 'D':
+                    nums[i] = 500;
+                    break;
+                case 'C':
+                    nums[i] = 100;
+                    break;
+                case 'L':
+                    nums[i] = 50;
+                    break;
+                case 'X':
+                    nums[i] = 10;
+                    break;
+                case 'V':
+                    nums[i] = 5;
+                    break;
+                case 'I':
+                    nums[i] = 1;
+                    break;
             }
         }
-        
-        return total;
+
+        for(int i=0; i<nums.length-1; i++) {
+            if(nums[i]<nums[i+1]) {
+                sum = sum - nums[i];
+            } else {
+                sum = sum + nums[i];
+            }
+        }
+
+        return sum + nums[nums.length-1];
     }
 }
