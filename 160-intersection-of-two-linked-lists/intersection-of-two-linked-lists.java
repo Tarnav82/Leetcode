592a3fd80d861cc007d80b076c016496c2602629
Trespasser <arnavtiwari3513@gmail.com>
2026-09-07 22:24:53 +0530

    Time: 1 ms (99.92%) | Memory: 52.6 MB (88.74%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -14,14 +14,14 @@ public class Solution {
         if (headA == null || headB == null) {
             return null;
         }
-ListNode A = headA;
-ListNode B = headB;
-while(A!=B){
-    A = (A== null) ? headB:A.next;
-    B = (B== null) ? headA:B.next;
-}
-return B;
-     
-        
+        ListNode A = headA;
+        ListNode B = headB;
+        while(A!=B){
+            A = (A== null) ? headB:A.next;
+            B = (B== null) ? headA:B.next;
+        }
+        return B;
+            
+                
     }
 }
